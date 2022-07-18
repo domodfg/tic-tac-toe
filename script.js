e7a3b7f8edbbb59c78127c5384be3d5cb032d70b
domodfg <domodfg@yahoo.com.hk>
2022-07-19 02:09:18 +0800

    Add function to update the gameboard array when user put their signs

--- script.js
+++ script.js
@@ -3,32 +3,34 @@ const playerFactory = (name, sign) => {
 };
 
 const gameBoard = (() => {
+  const player1 = playerFactory("player1", "X");
+  const player2 = playerFactory("player2", "O");
   const Array = ["", "", "", "", "", "", "", "", ""];
   return {
     Array,
+    player1,
+    player2
   };
 })();
 
 const displayController = (() => {
-  const player1 = playerFactory("player1", "X");
-  const player2 = playerFactory("player2", "O");
   let player;
   const gameBoardDisplay = document.querySelector(".gameBoard");
   const displayArray = () => {
-    console.log(gameBoard.player);
     for (let i = 0; i < gameBoard.Array.length; i++) {
       let XO = document.createElement("div");
       XO.textContent = gameBoard.Array[i];
       XO.setAttribute("class", "XO");
       XO.setAttribute("id", i);
-      XO.addEventListener("click", () => {
+      XO.addEventListener("click", (e) => {
         if (XO.textContent == "") {
-          if (player == player1) {
-            player = player2;
+          if (player == gameBoard.player1) {
+            player = gameBoard.player2;
           } else {
-            player = player1;
+            player = gameBoard.player1;
           }
           XO.textContent = player.sign;
+          gameBoard.Array[e.target.id]= player.sign;
         }
       });
       gameBoardDisplay.appendChild(XO);
