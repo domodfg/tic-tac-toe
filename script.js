71ee02e38f1af021be262f6a5246985e29c42edc
domodfg <domodfg@yahoo.com.hk>
2022-07-19 01:52:53 +0800

    Add a function to let player click the DOM and display their moves

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
+const playerFactory = (name, sign) => {
+  return { name, sign };
+};
+
 const gameBoard = (() => {
-  const Array = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+  const Array = ["", "", "", "", "", "", "", "", ""];
   return {
     Array,
   };
 })();
 
-const playerFactory = (name) => {
-  return { name };
-};
-
 const displayController = (() => {
+  const player1 = playerFactory("player1", "X");
+  const player2 = playerFactory("player2", "O");
+  let player;
   const gameBoardDisplay = document.querySelector(".gameBoard");
-  const generateContents = () => {
+  const displayArray = () => {
+    console.log(gameBoard.player);
     for (let i = 0; i < gameBoard.Array.length; i++) {
-      let content = document.createElement("div");
-      content.textContent=gameBoard.Array[i];
-      content.setAttribute('class', 'XO')
-      gameBoardDisplay.appendChild(content);
+      let XO = document.createElement("div");
+      XO.textContent = gameBoard.Array[i];
+      XO.setAttribute("class", "XO");
+      XO.setAttribute("id", i);
+      XO.addEventListener("click", () => {
+        if (XO.textContent == "") {
+          if (player == player1) {
+            player = player2;
+          } else {
+            player = player1;
+          }
+          XO.textContent = player.sign;
+        }
+      });
+      gameBoardDisplay.appendChild(XO);
     }
   };
-  return { generateContents };
+  return { displayArray };
 })();
