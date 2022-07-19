92c30900a81e56ef020c471a6ccc5be59a3640f6
domodfg <domodfg@yahoo.com.hk>
2022-07-19 18:01:41 +0800

    Add a function to display the winner when the game is over

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const gameBoard = (() => {
   const Array = ["", "", "", "", "", "", "", "", ""];
   const chooseWinner = () => {
     if (Array[i] == player1.sign) {
-      console.log(`${player1.name} win`);
-    } else console.log(`${player2.name} win`);
+      player1.winStatus = "win";
+    } else player2.winStatus = "win";
   };
   const judge = () => {
     for (i = 0; i <= 6; i += 3) {
@@ -47,7 +47,7 @@ const gameBoard = (() => {
       ) {
         chooseWinner();
       }
-    } 
+    }
   };
   return {
     Array,
@@ -60,6 +60,13 @@ const gameBoard = (() => {
 const displayController = (() => {
   let player;
   const gameBoardDisplay = document.querySelector(".gameBoard");
+  const result = document.querySelector(".result");
+  const displayWinner = () => {
+    if (gameBoard.player1.winStatus == "win") {
+      result.textContent = gameBoard.player1.name + " win";
+    } else if (gameBoard.player2.winStatus == "win")
+      result.textContent = gameBoard.player2.name + " win";
+  };
   const displayArray = () => {
     for (let i = 0; i < gameBoard.Array.length; i++) {
       let XO = document.createElement("div");
@@ -67,7 +74,11 @@ const displayController = (() => {
       XO.setAttribute("class", "XO");
       XO.setAttribute("id", i);
       XO.addEventListener("click", (e) => {
-        if (XO.textContent == "") {
+        if (
+          XO.textContent == "" &&
+          gameBoard.player1.winStatus != "win" &&
+          gameBoard.player2.winStatus != "win"
+        ) {
           if (player == gameBoard.player1) {
             player = gameBoard.player2;
           } else {
@@ -76,23 +87,27 @@ const displayController = (() => {
           XO.textContent = player.sign;
           gameBoard.Array[e.target.id] = player.sign;
           gameBoard.judge();
+          displayWinner();
         }
       });
       gameBoardDisplay.appendChild(XO);
     }
   };
   const clearBoard = () => {
-    player=gameBoard.player2;
-    const allXO= document.querySelectorAll('.XO');
-    allXO.forEach((XO)=> XO.textContent='');
+    player = gameBoard.player2;
+    result.textContent = "";
+    gameBoard.player1.winStatus = "";
+    gameBoard.player2.winStatus = "";
+    const allXO = document.querySelectorAll(".XO");
+    allXO.forEach((XO) => (XO.textContent = ""));
     for (let i = 0; i < gameBoard.Array.length; i++) {
-      gameBoard.Array[i]='';
+      gameBoard.Array[i] = "";
     }
-  }
-  return { displayArray , clearBoard};
+  };
+  return { displayArray, clearBoard };
 })();
 
-const play= document.querySelector('.play');
-const reset= document.querySelector('.reset');
-play.addEventListener('click', displayController.displayArray);
-reset.addEventListener('click', displayController.clearBoard);
+const play = document.querySelector(".play");
+const reset = document.querySelector(".reset");
+play.addEventListener("click", displayController.displayArray);
+reset.addEventListener("click", displayController.clearBoard);
